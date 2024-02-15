e460110d1bfa9b0a918378dbdfbe5de47ed884ed
Nathan Jeanbourquin <njeanbou@c1r16s1.42lausanne.ch>
2024-02-15 16:30:18 +0100

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: njeanbou <njeanbou@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 15:34:49 by njeanbou          #+#    #+#             */
+/*   Updated: 2024/02/15 16:22:34 by njeanbou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; argv[i]; i++)
+		{
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char) toupper(argv[i][j]);
+		}
+		std::cout << std::endl;
+	}
+	return (EXIT_SUCCESS);
+}
